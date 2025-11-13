71abc690a53ab37e92001eeffd195e564024f1ba
makinifab <makinifarbean@gmail.com>
2025-11-13 13:34:39 +0300

    url2

--- src/api/auth.js
+++
@@ -1,5 +0,0 @@
-import API from './axios';
-
-export const login = (email, password) => API.post('/auth/login', { email, password });
-export const register = (data) => API.post('/auth/register', data);
-export const logout = () => localStorage.removeItem('token');
